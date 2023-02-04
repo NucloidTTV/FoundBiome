185f4a3ae8a0adfa8b5f8b03093a8fb772c3488d
William Bakunis <wbakunis@yahoo.com>
2023-02-04 12:19:10 -0500

    refactor
    Considerable changes made for updating to latest spigotmc.

--- src/main/java/net/alureon/foundbiome/file/FileHandler.java
+++ src/main/java/net/alureon/foundbiome/file/FileHandler.java
@@ -4,36 +4,34 @@ import net.alureon.foundbiome.FoundBiome;
 
 public class FileHandler {
 
+	private FoundBiome fb;
+	private String worldName;
+	private boolean broadcast;
 
-    private FoundBiome fb;
-    private String worldName;
-    private boolean broadcast;
+	public FileHandler(FoundBiome fb) {
+		this.fb = fb;
+	}
 
+	public void checkFiles() {
+		if (!fb.getDataFolder().exists()) {
+			boolean dirsMade = fb.getDataFolder().mkdirs();
+			if (!dirsMade) {
+				fb.getServer().getLogger().severe("Failed to make plugin directory!");
+				fb.getServer().getLogger().severe("This will probably not work correctly.");
+			}
+		}
+		fb.getConfig().options().copyDefaults(true);
+		fb.saveConfig();
 
-    public FileHandler(FoundBiome fb) {
-        this.fb = fb;
-    }
+		this.worldName = fb.getConfig().getString(Paths.worldName);
+		this.broadcast = fb.getConfig().getBoolean(Paths.broadcast);
+	}
 
-    public void checkFiles() {
-        if (!fb.getDataFolder().exists()) {
-            boolean dirsMade = fb.getDataFolder().mkdirs();
-            if (!dirsMade) {
-                fb.getServer().getLogger().severe("Failed to make plugin directory!");
-                fb.getServer().getLogger().severe("This will probably not work correctly.");
-            }
-        }
-        fb.getConfig().options().copyDefaults(true);
-        fb.saveConfig();
+	public String getWorldName() {
+		return this.worldName;
+	}
 
-        this.worldName = fb.getConfig().getString(Paths.worldName);
-        this.broadcast = fb.getConfig().getBoolean(Paths.broadcast);
-    }
-
-    public String getWorldName() {
-        return this.worldName;
-    }
-
-    public boolean getBroadcast() {
-        return this.broadcast;
-    }
+	public boolean getBroadcast() {
+		return this.broadcast;
+	}
 }
--- src/main/java/net/alureon/foundbiome/file/Paths.java
+++ src/main/java/net/alureon/foundbiome/file/Paths.java
@@ -2,7 +2,7 @@ package net.alureon.foundbiome.file;
 
 final class Paths {
 
-    final static String broadcast    = "FoundBiome Configuration.Broadcast";
-    final static String worldName    = "FoundBiome Configuration.World Name";
+	final static String broadcast = "FoundBiome Configuration.Broadcast";
+	final static String worldName = "FoundBiome Configuration.World Name";
 
 }
